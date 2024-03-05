23484b9c0a30455ffd3de58e2a2366e731263c73
jelliness <jellyannekaye0504@gmail.com>
2024-03-05 20:47:21 +0800

    Menu Page

---
+++ Final_Revelation/Assets/Scripts/Menu_Script.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Script : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void GotoPlayMenu()
+    {
+        SceneManager.LoadScene("PlayMenu");
+    }
+    public void GotoMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
